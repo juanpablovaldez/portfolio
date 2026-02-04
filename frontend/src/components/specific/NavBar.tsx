66fffd60ba24ca9a85c24bc90e985347414b0cac
Orbitado <juanpaavaldezz@gmail.com>
2026-02-04 13:11:54 -0300

    feat: Refactor NavBar component to simplify section visibility logic and remove data fetching

--- frontend/src/components/specific/NavBar.tsx
+++ frontend/src/components/specific/NavBar.tsx
@@ -2,26 +2,14 @@ import ThemeSwitcher from "./ThemeSwitcher";
 import NavToggler from "./NavToggler";
 import NavList from "./NavList";
 import Link from "next/link";
-import { getStrapiData } from "@/lib/strapi";
 
-async function NavBar() {
-  const [experiences, educations, certifications, projects, skills] =
-    await Promise.all([
-      getStrapiData<WorkExperience[]>("work-experiences?populate=*&sort=order"),
-      getStrapiData<Education[]>("educations?populate=*&sort=order"),
-      getStrapiData<Certification[]>("certifications?sort=order"),
-      getStrapiData<StrapiProject[]>("projects?populate=*&sort=order"),
-      getStrapiData<Skill[]>("skills?sort=order"),
-    ]);
-
+function NavBar() {
   const availableSections = {
     summary: true,
-    experience: experiences && experiences.length > 0,
-    education:
-      (educations && educations.length > 0) ||
-      (certifications && certifications.length > 0),
-    projects: projects && projects.length > 0,
-    skills: skills && skills.length > 0,
+    experience: true,
+    education: true,
+    projects: true,
+    skills: true,
     contact: true,
   };
 
